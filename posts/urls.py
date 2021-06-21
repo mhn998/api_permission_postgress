e0a1755cf6c7c1d411186269067101d66fa8ebc4
mhn998 <muhannadalmughrabi233@gmail.com>
2021-06-21 23:07:23 +0300

    initial commit

---
+++ posts/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import PostList, PostDetail
+
+urlpatterns = [
+    path('', PostList.as_view(), name='posts_list'),
+    path('<int:pk>/', PostDetail.as_view(), name='posts_detail'),
+]
